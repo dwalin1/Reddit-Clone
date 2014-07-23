abbb53ad0b0da024a66a6426a982ad30f9dfaf0a
Daniel Walin <dwalin1@gmail.com>
2014-07-23 11:58:57 -0700

    Use Faker to get lots of seed data to play with

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,30 @@
 # end
 
 10.times do
-  User.create!(
-    username: Faker::Internet.user_name,
-    password: "password"
-  )  
+  ActiveRecord::Base.transaction do   
+    u = User.create!(
+      username: Faker::Internet.user_name,
+      password: "password"
+    )
+  
+    Sub.create!(
+      title: Faker::Lorem.words(3).join(" "),
+      description: Faker::Lorem.sentence,
+      moderator_id: u.id
+    )  
+  end
+end
+
+User.all.each do |user|
+  Sub.all.each do |sub|
+    Post.create!(
+      title: Faker::Lorem.words(5).join(" "),
+      url: Faker::Internet.url,
+      content: Faker::Lorem.sentence,
+      sub_id: sub.id,
+      submitter_id: user.id
+    )
+  end
 end
 
 
