f9e1ecbcb4591e743e655f0f64a6393845f7efe4
Daniel Walin <dwalin1@gmail.com>
2014-07-24 13:47:11 -0700

    Can delete comments. Only commenter sees edit/delete buttons.

--- app/assets/javascripts/views/posts/postShow.js
+++ app/assets/javascripts/views/posts/postShow.js
@@ -2,11 +2,13 @@ App.Views.postShow = Backbone.View.extend({
 	template: JST["posts/postShow"],
 	
 	initialize: function() {
-		this.listenTo(this.model, "sync", this.render);	
+		this.listenTo(this.model, "sync", this.render);
+		this.listenTo(this.model.comments(), "add remove", this.render);
 	},
 	
 	events: {
 		"click button.deletePost": "deletePost",
+		"click button.deleteComment": "deleteComment",
 		"submit form.commentForm": "postComment"
 	},
 	
@@ -18,7 +20,8 @@ App.Views.postShow = Backbone.View.extend({
 		return this;
 	},
 	
-	deletePost: function() {
+	deletePost: function(event) {
+		event.preventDefault();
 		var sub_id = this.model.get("sub_id");
 		this.model.destroy();
 		App.router.navigate("#subs/" + sub_id, { trigger: true });
@@ -33,12 +36,19 @@ App.Views.postShow = Backbone.View.extend({
 		comment.save({}, {
 			success: function(model, response) {
 				console.log("Success!");
-				that.model.fetch();
+				that.model.comments().add(model);
 			},
 			
 			error: function(model, response) {
 				console.log("Error!");
 			}
 		})
+	},
+	
+	deleteComment: function(event) {
+		event.preventDefault();
+		var comment_id = $(event.target).data("id");
+		var comment = this.model.comments().get(comment_id);
+		comment.destroy();
 	}
 });
